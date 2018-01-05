471a6d9e63a227bb8d99700520d94e4a76f2b604
shubham <shubhamsoni136@gmail.com>
2018-01-06 01:26:18 +0530

    api updated

--- server.js
+++ server.js
@@ -9,7 +9,23 @@ app.get('/',function(req,res){
   res.send('shubham');
 });
 app.get('/nextstation',function(req,res){
-  
+  var i=0;
+  var key = "cmd6bebf4c";
+  var tno = "19665";
+  var date = "05-01-2018";
+  var url = "https://api.railwayapi.com/v2/live/train/"+ tno +"/date/"+date+"/apikey/"+key;
+  request({url:url,json:true}, function(error , response , body){
+    console.log('error:',error);
+    console.log('statusCode:',response && response.statusCode);
+    var data = JSON.parse(JSON.stringify(body));
+    var rt = data.route;
+    for(i=0; i<rt.length ; i++){
+      if(rt[i].has_departed===false){
+        break;
+      }
+    }
+    res.send(rt[i].station.name);
+  });
 });
 var port = 8080;
 app.listen(port,function(){
